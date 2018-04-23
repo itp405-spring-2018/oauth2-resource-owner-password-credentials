f4b4aa64acb063f010e3c0ae133b9acd5cc08076
David Tang <dtang859@gmail.com>
2018-04-22 22:57:24 -0700

    implemented the password grant type where middleware verifies token in Authorization header before passing along the request to the route handlers

--- app.js
+++ app.js
@@ -2,13 +2,68 @@ const express = require('express');
 const cors = require('cors');
 const Genre = require('./models/Genre');
 const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 
+// move to .env file
+const SECRET_KEY = 'mysecretkey';
+
+app.post('/oauth/token', (request, response) => {
+  response.set({
+    'Cache-Control': 'no-store',
+    'Pragma': 'no-cache'
+  });
+
+  let { username, password, grant_type: grantType } = request.body;
+
+  if (grantType !== 'password') {
+    response.status(400).json({
+      error: 'unsupported_grant_type',
+      error_description: '',
+      error_uri: 'https://www.oauth.com/oauth2-servers/access-tokens/access-token-response/'
+    });
+  }
+
+  if (username === 'admin' && password === '123') {
+    let token = jwt.sign({ username }, SECRET_KEY);
+
+    response.json({
+      access_token: token,
+      token_type: 'bearer',
+    });
+  }
+});
+
+app.use((request, response, next) => {
+  let [ tokenType, token ] = request.get('Authorization').split(' ');
+
+  if (tokenType !== 'Bearer') {
+    response.status(401).json({
+      error: 'invalid_client'
+    });
+  } else {
+    jwt.verify(token, SECRET_KEY, (err, decoded) => {
+      if (!err) {
+        request.user = decoded;
+        next();
+      } else {
+        response.status(401).json({
+          error: 'invalid_client',
+          // adding details for debugging
+          details: err
+        });
+      }
+    });
+  }
+});
+
 app.get('/api/genres', function(request, response) {
-  Genre.fetchAll().then(function(genres) {
+  console.log('user', request.user);
+  Genre.fetchAll().then((genres) => {
     response.json(genres);
   });
 });
@@ -16,21 +71,18 @@ app.get('/api/genres', function(request, response) {
 app.get('/api/genres/:id', function(request, response) {
   let { id } = request.params;
   new Genre({ GenreId: id }).fetch()
-    .then(function(genre) {
+    .then((genre) => {
       if (!genre) {
         throw new Error(`Genre ${id} not found`);
       } else {
         response.json(genre);
       }
     })
-    .catch(function(error) {
+    .catch((error) => {
       response.status(404).json({
         error: error.message
       });
     });
 });
 
-const port = process.env.PORT || 8000;
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
-});
+app.listen(process.env.PORT || 8000);
