849b1108638c5408ba779fba1e77b591aada047b
David Tang <dtang859@gmail.com>
2018-04-24 09:48:29 -0700

    starter

--- app.js
+++ app.js
@@ -2,75 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const Genre = require('./models/Genre');
 const bodyParser = require('body-parser');
-const jwt = require('jsonwebtoken');
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
-
-// move to .env file
-const SECRET_KEY = 'mysecretkey';
-
-app.post('/oauth/token', (request, response) => {
-  response.set({
-    'Cache-Control': 'no-store',
-    'Pragma': 'no-cache'
-  });
-
-  let { username, password, grant_type: grantType } = request.body;
-
-  if (grantType !== 'password') {
-    response.status(400).json({
-      error: 'unsupported_grant_type',
-      error_description: '',
-      error_uri: 'https://www.oauth.com/oauth2-servers/access-tokens/access-token-response/'
-    });
-  }
-
-  if (username === 'admin' && password === '123') {
-    let token = jwt.sign({ username }, SECRET_KEY, { expiresIn: '15m' });
-
-    // store token in database
-
-    response.json({
-      access_token: token,
-      token_type: 'bearer',
-    });
-  }
-});
-
-app.use((request, response, next) => {
-  let authorizationHeader = request.get('Authorization');
-
-  if (!authorizationHeader) {
-    return response.status(401).json({
-      error: 'invalid_client'
-    });
-  }
-
-  let [ tokenType, token ] = authorizationHeader.split(' ');
-
-  if (tokenType !== 'Bearer') {
-    response.status(401).json({
-      error: 'invalid_client'
-    });
-  } else {
-    jwt.verify(token, SECRET_KEY, (err, decoded) => {
-      if (!err) {
-        // verify token in database
-        request.user = decoded;
-        next();
-      } else {
-        response.status(401).json({
-          error: 'invalid_client',
-          // adding details for debugging
-          details: err
-        });
-      }
-    });
-  }
-});
 
 app.get('/api/genres', function(request, response) {
   console.log('user', request.user);
